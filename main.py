30ec99761169ba94d0c61dd1fc59019e35503b10
Polo4722valentina <vergelesvalentina07@gmail.com>
2023-10-30 19:41:40 +0200

    Solution

--- main.py
+++ main.py
@@ -7,4 +7,47 @@
 
 class Transport:
     # введіть свій код тут
-    pass
+    def __init__(self, speed, power):
+        self.speed = speed 
+        self.power = power
+
+    def accelerate(self, amount):   
+        self.speed += amount 
+
+
+class Car(Transport):
+    def __init__(self, speed, power, fuel):
+        super().__init__(speed, power)
+        self.fuel = fuel 
+
+
+class Bicycle(Transport):     
+    def __init__(self, speed, power, pedals):
+        super().__init__(speed, power)
+        self.pedals = pedals
+
+
+car = Car(100, 200, "gasoline") 
+bicycle = Bicycle(20, 50, 2)      
+
+car.accelerate(30)
+bicycle.accelerate(10)
+
+
+import unittest
+from main import Transport, Car, Bicycle
+
+class TestTransportMethods(unittest.TestCase):
+
+    def test_accelerate(self):
+        car = Car(100, 200, "gasoline")
+        bicycle = Bicycle(20,50, 2)
+
+        car.accelerate(30)
+        bicycle.accelerate(10)
+
+        self.assertEqual(car.speed, 130)
+        self.assertEqual(bicycle.speed, 30)
+
+if __name__ == ' __main__ ':
+    unittest.main()        
